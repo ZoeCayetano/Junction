43ea13b6836de6c0cfbad4029175485adcbafb0d
Zoë Cayetano <zcayetan@asu.edu>
2016-11-26 15:37:43 +0200

    Add music player asset

---
+++ Assets/MusicManager/Scripts/MusicManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+// using MusicPlayer;
+
+	public class MusicManager : MonoBehaviour
+	{
+		// The music player game object
+		private static GameObject me = null;
+		private static MusicPlayer mp = null;
+		
+		// Retreive or create the music emitter
+		public static GameObject getMusicEmitter()
+		{
+			if(me == null)
+			{
+				me = new GameObject();
+				me.name = "Music Emitter";
+				me.AddComponent<AudioSource>();
+				me.GetComponent<AudioSource>().loop = true;
+				DontDestroyOnLoad(mp);
+				mp = me.AddComponent<MusicPlayer>();
+			}
+			return me;
+		}
+		
+		// Retreive or create the music player component of the emitter
+		public static MusicPlayer getMusicPlayer()
+		{
+			if(mp == null)
+			{
+				mp = getMusicEmitter().GetComponent<MusicPlayer>();
+				if(mp == null)
+				{
+					mp = me.AddComponent<MusicPlayer>();
+				}
+			}
+			return mp;
+		}
+		
+		// Play a music
+		public static void play(AudioClip clip, float fadeOut, float fadeIn)
+		{
+			getMusicPlayer().playMusic(clip, fadeOut, fadeIn);
+		}
+
+		// Play a music with a filename in a Resources folder
+		public static void play(string name)
+		{
+			AudioClip a = (AudioClip)Resources.Load(name, typeof(AudioClip));
+			if(a != null){
+			play(a, getMusicPlayer().getFadeOut(), getMusicPlayer().getFadeIn());
+			}
+			else
+			{
+				Debug.Log("Sorry, I could not find \""+name+"\".");
+			}
+		}
+		
+		// Set if the music should loop or not
+		public static void setLoop(bool t)
+		{
+			getMusicEmitter().GetComponent<AudioSource>().loop = t;
+		}
+		
+		// Pause the music
+		public static void pause()
+		{
+			getMusicPlayer().pauseMusic();
+		}
+		
+		// Unpause the music
+		public static void unpause()
+		{
+			getMusicPlayer().unpauseMusic();
+		}
+		
+		// Stop the music
+		public static void stop(float fadeOut = 0f)
+		{
+			getMusicPlayer().stopMusic(fadeOut);
+		}
+		
+		// Set the volume of the music
+		public static void setVolume(float volume = 1.0f, float duration = 0f)
+		{
+			getMusicPlayer().setMusicVolume(volume, duration);
+		}
+	}
---
+++ Assets/MusicManager/TestScene/ExampleScript.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleScript : MonoBehaviour
+{
+	void OnGUI()
+	{
+		int y = 10;
+		int stepy = 30;
+		int width = 250;
+		int height = 20;
+		
+		if(GUI.Button(new Rect(10, y, width, height), "Play Music 1"))
+		{
+			MusicManager.play("Dark Descent");
+		}
+		
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Play Music 2"))
+		{
+			MusicManager.play("The Dark Amulet");
+		}
+
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Play Music 3"))
+		{
+			MusicManager.play("Dark Descent");
+		}
+
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Play Music 4"))
+		{
+			MusicManager.play("The Dark Amulet");
+		}
+
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Play Music 5"))
+		{
+			MusicManager.play("Dark Descent");
+		}
+		
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Pause"))
+		{
+			MusicManager.pause();
+		}
+		
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Unpause/Play"))
+		{
+			MusicManager.unpause();
+		}
+		
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Stop the music"))
+		{
+			MusicManager.stop(0.0f);
+		}
+		
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Set volume to 25%"))
+		{
+			MusicManager.setVolume(0.25f, 1.0f);
+		}
+		
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Set volume to 50%"))
+		{
+			MusicManager.setVolume(0.50f, 1.0f);
+		}
+
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Set volume to 75%"))
+		{
+			MusicManager.setVolume(0.75f, 1.0f);
+		}
+
+		y += stepy;
+		if(GUI.Button(new Rect(10, y, width, height), "Set volume to 100%"))
+		{
+			MusicManager.setVolume(1.0f, 1.0f);
+		}
+		
+		// Debug purposes
+		// Also demonstrates how to get the gameObject which hold the music
+		GameObject player = MusicManager.getMusicEmitter();
+		if(player.GetComponent<AudioSource>().clip != null)
+		{
+			GUI.Label(new Rect(20+width, 10, 200, 30), "Song playing: "+player.GetComponent<AudioSource>().clip.name);
+			GUI.Label(new Rect(20+width, 40, 200, 30), "Volume: "+player.GetComponent<AudioSource>().volume);
+			GUI.Label(new Rect(20+width, 70, 200, 30), "Is playing: "+player.GetComponent<AudioSource>().isPlaying);
+		}
+	}
+}
